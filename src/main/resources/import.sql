9a006dfdd2471d453c81db7a73668d13f02852ce
Luis Guisado <lguisadom@gmail.com>
2021-07-11 00:40:18 -0500

    Se agrega import.sql para insertar datos de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony', 700, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple', 1000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Notebook', 1000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Hewlett Packard', 200, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Bianchi', 5000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Nike', 100, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Adidas', 123, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Reebok', 962, NOW());
